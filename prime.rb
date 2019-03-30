b21aa5bc15852abbc88e735293874dd7ce0ad5b3
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-03-30 22:07:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+def prime?(number)
+
+  divisor = 1
+  times = 0
+
+  while divisor <= number
+    if number % divisor == 0
+      times += 1
+    end
+    
+    if times > 2
+      break
+    end
+
+    divisor += 1
+  end
+
+  if times == 2
+    true
+  else
+    false
+  end
+end
